74c959baa4f655360e981a553f297c03d8d81a39
Eleanor Howard <ellie@lizthedeveloper.com>
2017-02-27 20:56:09 -0800

    changed dream seeds to real dreams

--- seeds/002dreams.js
+++ seeds/002dreams.js
@@ -6,29 +6,29 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('dreams').insert([
           {
-            name: 'Galvanize is really the matrix',
-            description: 'On-demand sand castle construction expertise.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?',
+            name: 'Galvanize is really the matrix.',
+            description: 'I had a dream where I was working on the computers in the lab at Galvanize and when my cohort would work on the computers I remember feeling like the monitors sucked us into a train ride somewhere else outside that would not stop and I remember trying to help someone that was falling off and they dragged themselves up, despite the pain they were going through trying to get back on the train. Later we were on the train again and that same person got left behind. When we got to our destination we found ourselves inside giant containers full of some kind of purple gel. It was much like when Neo woke up in the matrix, only in a very green area outdoors. Maybe a patio that reminded me of some kind of mario level. I saw my sister and she looked at me and told me with her eyes that she knew all this had been going on and she was helping. I felt shocked.',
             dream_image_url: 'images/dreams/guard.jpg',
             date: '2/22/17',
             user_id: 3
           },
           {
-            name: 'Michael was a kid again',
-            description: 'Earn points when your favorite politicians pass legislation. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?',
+            name: 'Spiders were everywhere!',
+            description: 'I dreamed that my sister and I were trapped in my old house and it was filled with spiders. They would come in waves. We had to escape while the spiders were gone, and hide under tissues in my closet if the spiders came out. They would crawl over everything. I remember looking for my sister when the spiders left but I could not find her. I woke up before making it outdoors.',
             dream_image_url: 'images/dreams/lilmichael.jpg',
             date: '2/23/17',
             user_id: 1
           },
           {
             name: 'my sister almost melted',
-            description: 'We already have your measurements and shipping address. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?',
+            description: 'I dreamed my sister was behind a couch in the computer room. If she were to fall all the way behind the couch I knew that she should melt. I kept trying to pull her up but she was hard to pull. Then a clone of her was in her room at the doorway, telling me to come play with her. I felt like she was a distraction from me saving my sister from melting. I woke up before anything really happened.',
             dream_image_url: 'images/dreams/sista.jpg',
             date: '2/24/17',
             user_id: 1
           },
           {
             name: 'Inside of a blackhole',
-            description: 'High-minded or absent-minded? You decide. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?',
+            description: 'What happens in a blackhole? what happens when you go into a blackhole in dreamland? Try it in your next lucid dream and let me know!',
             dream_image_url: 'images/dreams/spaceee.jpeg',
             date: '2/25/17',
             user_id: 2
